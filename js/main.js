45acf90abddd7d725a06383de4a54573dc800982
Su Boat <zuza.zagorska@gmail.com>
2020-03-19 15:12:44 +0100

    Cleanup push.

--- js/main.js
+++ js/main.js
@@ -3,19 +3,17 @@ var scrollItem = document.querySelectorAll('.stick');
 
 const handleScroll = (evt) =>
 {
-    let sectionId = evt.srcElement.className.split(" ", 1);
-    document.getElementById(`${sectionId}`).scrollIntoView({behaviour: 'smooth'});
-    // console.log(evt.srcElement.className.split(" ", 1));
 
-    // console.log(scrollItem.length);
+    let sectionId = evt.target.className.split(" ", 1);
+    document.getElementById(`${sectionId}`).scrollIntoView({behaviour: 'smooth'});
+
     for(let i=0;i<scrollItem.length;i++) 
     {
         scrollItem[i].classList.remove('stick-active');
     }
 
-    evt.srcElement.classList.add('stick-active')
+    evt.target.classList.add('stick-active');
 
-    console.log(evt.srcElement);
 }
 
 scroll.addEventListener('click', handleScroll);
